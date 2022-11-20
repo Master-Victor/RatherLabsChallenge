97637ad8022030c4d8f857a58ad2aa0ba7df9ecb
Victor Villafañe <vvillafane@est.frba.utn.edu.ar>
2022-11-20 01:25:48 -0300

    quiz - se agrego una card para poder visualizar las quiz diarias

--- pages/QuizHome.tsx
+++ pages/QuizHome.tsx
@@ -1,12 +1,26 @@
 import { useRouter } from 'next/router'
 import React from 'react'
 import LayoutQuiz from '../components/LayoutQuiz'
+import { Layout, Row, Col, Input, Slider, Button, Card } from 'antd';
+const { Header, Footer, Content } = Layout;
+
+const gridStyle: React.CSSProperties = {
+
+    textAlign: 'center',
+  };
+
 const QuizHome = () => {
     const router = useRouter()
-    console.log(router.query)
     return (
         <LayoutQuiz>
-            QuizHome
+            <div style={{ paddingLeft: '10vw', paddingTop: '10vh' }}>
+                <Card title={'title quiz'} style={{ width: '60vw', height: '50vh' }}>
+                    <Card.Grid style={gridStyle}>Content</Card.Grid>
+                    <Card.Grid style={gridStyle}>Content</Card.Grid>
+                    <Card.Grid style={gridStyle}>Content</Card.Grid>
+                    <Card.Grid style={gridStyle}>Content</Card.Grid>
+                </Card>
+            </div>
         </LayoutQuiz>
     )
 }
