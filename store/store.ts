c0017b3850b827521111d0cf9a6d593b70bf0140
Victor Villafañe <vvillafane@est.frba.utn.edu.ar>
2022-11-21 03:05:09 -0300

    Fix name - se corriguio el nombre de user por User en store.ts

--- store/store.ts
+++ store/store.ts
@@ -1,5 +1,5 @@
 import { RootStore, IRootStore } from './models/Quiz'
-import { User, IUserStore } from './models/user'
+import { User, IUserStore } from './models/User'
 
 let _StoreQuiz: IRootStore
 let _StoreUser: IUserStore
