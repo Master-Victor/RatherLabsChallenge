06b733347def6520ed99261ef300331c291a4f56
Master-Victor <vvillafane@frba.utn.edu.ar>
2022-12-02 04:15:40 -0300

    Fix - Se cambio la configuracion de NextJS para que no renderize dos veces los componentes en desarrollo

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   swcMinify: true,
   images: { domains: ['interactive-examples.mdn.mozilla.net', '48tools.com'], formats: ['image/avif', 'image/webp'], }
 }
